ebc7344fe86e7060b28d4343c0f5eb17844930d5
Tuan Le <tlna95@icloud.com>
2023-04-20 07:45:46 -0400

    push push

--- Service Master/Booking.swift
+++ Service Master/Booking.swift
@@ -21,6 +21,23 @@ class Booking: ObservableObject, Identifiable {
     @Published var email: String
     @Published var method: String
     @Published var service: Service
+    
+    init(_ booking: Dictionary<String, Any>) {
+        self.uid = booking["uid"] as? String ?? ""
+        self.postalCode = booking["postalCode"] as? String ?? ""
+        self.urgency = booking["urgency"] as? String ?? ""
+        self.description = booking["description"] as? String ?? ""
+        self.fullName = booking["fullName"] as? String ?? ""
+        self.address = booking["address"] as? String ?? ""
+        self.city = booking["city"] as? String ?? ""
+        self.province = booking["province"] as? String ?? ""
+        self.contactNumber = booking["contactNumber"] as? String ?? ""
+        self.email = booking["email"] as? String ?? ""
+        self.method = booking["method"] as? String ?? ""
+        self.service = Service(booking["service"] as! NSDictionary)
+    }
+    
+    
     init() {
         uid = ""
         postalCode = ""
